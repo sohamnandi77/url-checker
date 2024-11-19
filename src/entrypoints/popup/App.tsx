4d1b9a0f28077f34d84588af5749f362839fb568
Soham Nandi <sohamnandi77@gmail.com>
2024-11-19 16:52:47 +0800

    📦 build: install & setup shadcn/ui

--- src/entrypoints/popup/App.tsx
+++ src/entrypoints/popup/App.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function App() {
   return (
-    <div className="min-w-[400px] min-h-[400px] flex items-center justify-center">
-      <p className="text-3xl font-bold">Hello World!</p>
+    <div className="min-w-[400px] min-h-[400px] flex items-center justify-center px-5">
+      <Input type="text" placeholder="Type something..." />
+      <Button className="ml-2">Submit</Button>
     </div>
   );
 }
