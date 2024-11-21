1bd885f271abf689782caa2c1db85015c9bde510
Soham Nandi <sohamnandi77@gmail.com>
2024-11-21 11:01:15 +0800

    💄 style: add dark mode logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,16 @@ const Header = () => {
   return (
     <div className="flex h-16 w-full items-center justify-between bg-primary px-5">
       <div className="flex items-center gap-3">
-        <img src="/url-checker.png" alt="logo" className="h-8 w-8" />
+        <img
+          src="/url-checker.png"
+          alt="logo"
+          className="h-8 w-8 dark:hidden"
+        />
+        <img
+          src="/url-checker-dark.png"
+          alt="logo"
+          className="hidden h-8 w-8 dark:block"
+        />
         <h1 className="text-base font-extrabold text-secondary">URL Checker</h1>
       </div>
       <ModeToggle />
