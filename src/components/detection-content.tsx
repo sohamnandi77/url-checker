250d7092dc9f963c575ff9e95a2a59e952af3432
Soham Nandi <sohamnandi77@gmail.com>
2024-11-26 15:09:41 +0800

    ✨ feat: create detection content component

---
+++ src/components/detection-content.tsx
@@ -0,0 +1,71 @@
+import { CheckCircle2, CircleAlert, Info } from "lucide-react";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
+
+type AnalysisResult = {
+  method: string;
+  engine_name: string;
+  category: string;
+  result: string;
+};
+
+type LastAnalysisResults = {
+  [engine_name: string]: AnalysisResult;
+};
+
+interface DetectionContentProps {
+  analysis: LastAnalysisResults;
+}
+
+export const DetectionContent = ({ analysis }: DetectionContentProps) => {
+  return (
+    <>
+      <div className="my-1 flex items-center justify-center space-x-1.5 rounded-sm bg-gray-200 px-2 py-1 dark:bg-neutral-700">
+        <p className="text-center text-xs text-muted-foreground">
+          Security vendors' analysis
+        </p>
+        <TooltipProvider delayDuration={100}>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Info
+                size={16}
+                className="cursor-pointer text-muted-foreground hover:text-primary/95"
+              />
+            </TooltipTrigger>
+            <TooltipContent>
+              <p className="max-w-48 text-xs">
+                Security industry / community detections, or lack thereof, for
+                the reported domain or URL.
+              </p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
+      <div className="grid divide-y-[1px]">
+        {Object.keys(analysis).map((engineName, i) => (
+          <div
+            className="flex justify-between space-x-2 py-1.5 font-medium"
+            key={i}
+          >
+            <p className="text-xs text-muted-foreground">{engineName}</p>
+            <div className="flex items-center space-x-1.5">
+              {analysis[engineName].result === "clean" ? (
+                <CheckCircle2 size={16} className="text-green-500" />
+              ) : (
+                <CircleAlert size={16} className="text-red-500" />
+              )}
+              <p className="text-xs text-muted-foreground">
+                {analysis[engineName].result}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
