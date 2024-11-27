8e32f525b1f49b8e2a4a38b98bde0b054431c201
Soham Nandi <sohamnandi77@gmail.com>
2024-11-27 18:20:52 +0800

    ✨ feat: update the display on unshorten content component

--- src/components/unshorten-url-content.tsx
+++ src/components/unshorten-url-content.tsx
@@ -1,38 +1,75 @@
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { Undo2 } from "lucide-react";
+
 import { Button } from "@/components/ui/button";
+import { UnshortenUrlForm } from "@/components/unshorten-url-form";
+import { UnshortenUrlResult } from "@/components/unshorten-url-result";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
 import {
   Card,
   CardTitle,
-  CardFooter,
   CardHeader,
   CardContent,
   CardDescription,
 } from "@/components/ui/card";
 
 export const UnshortenUrlContent = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [data, setData] = useState<any>(null);
+
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Unshorten URL</CardTitle>
-        <CardDescription>
-          Enter the URL you want to unshorten here.
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="space-y-2">
-        <div className="space-y-1">
-          <Label htmlFor="unshortenurl">URL</Label>
-          <Input
-            type="url"
-            id="unshortenurl"
-            name="unshortenurl"
-            placeholder="https://shorturl.at/"
-          />
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button>Unshorten</Button>
-      </CardFooter>
-    </Card>
+    <>
+      {!data && (
+        <Card>
+          <CardHeader>
+            <CardTitle>Unshorten URL</CardTitle>
+            <CardDescription>
+              Enter the URL you want to unshorten here.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="space-y-2 p-6 pt-0">
+            <UnshortenUrlForm
+              loading={loading}
+              setLoading={setLoading}
+              setData={setData}
+            />
+          </CardContent>
+        </Card>
+      )}
+
+      {data && (
+        <Card className="relative dark:bg-muted/30">
+          <TooltipProvider delayDuration={100}>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant="ghost"
+                  size="icon"
+                  className="absolute right-2 top-2 h-6 w-6"
+                  onClick={() => setData(null)}
+                >
+                  <Undo2 />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>
+                <p className="max-w-48 text-xs">Reset the URL scan report.</p>
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+
+          <CardHeader className="p-4 pb-0">
+            <CardTitle>URL Unshorten Result</CardTitle>
+          </CardHeader>
+          <CardContent className="w-full p-4">
+            <UnshortenUrlResult data={data} />
+          </CardContent>
+        </Card>
+      )}
+    </>
   );
 };
