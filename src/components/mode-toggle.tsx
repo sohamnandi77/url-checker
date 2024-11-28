4613faed31a000ce0cc69fa6771b54e61451a876
Soham Nandi <sohamnandi77@gmail.com>
2024-11-28 13:36:15 +0800

    🚀 feat: add tooltip for mode toggle button

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,20 +1,36 @@
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+
 import { Button } from "@/components/ui/button";
 import { useTheme } from "@/components/theme-provider";
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
 
 export function ModeToggle() {
   const { setTheme, theme } = useTheme();
 
   return (
-    <Button
-      className="h-6 w-6 rounded-full"
-      variant="outline"
-      size="icon"
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-    >
-      <SunIcon className="h-4 w-4 rotate-90 scale-0 transition-transform duration-500 ease-in-out dark:rotate-0 dark:scale-100" />
-      <MoonIcon className="scale-1000 absolute h-4 w-4 rotate-0 transition-transform duration-500 ease-in-out dark:-rotate-90 dark:scale-0" />
-      <span className="sr-only">Switch Theme</span>
-    </Button>
+    <TooltipProvider delayDuration={100}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            className="h-6 w-6 rounded-full"
+            variant="outline"
+            size="icon"
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+          >
+            <SunIcon className="h-4 w-4 rotate-90 scale-0 transition-transform duration-500 ease-in-out dark:rotate-0 dark:scale-100" />
+            <MoonIcon className="scale-1000 absolute h-4 w-4 rotate-0 transition-transform duration-500 ease-in-out dark:-rotate-90 dark:scale-0" />
+            <span className="sr-only">Switch Theme</span>
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent className="bg-secondary">
+          <p className="max-w-48 text-xs text-primary">Swith theme</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
