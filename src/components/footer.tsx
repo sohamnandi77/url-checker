dc01b3bdad03087981b9462cff37308f5c106ffa
Soham Nandi <sohamnandi77@gmail.com>
2024-11-29 15:34:20 +0800

    🎨 feat: update footer content

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -14,22 +14,11 @@ export const Footer = () => {
     >
       <AccordionItem className="!border-none" value="item-1">
         <AccordionTrigger className="hover:no-underline">
-          <p className="text-sm text-muted-foreground dark:text-muted/70">
-            Created by{" "}
-            <a
-              className="text-sm font-semibold text-primary underline underline-offset-4 hover:text-primary/80 dark:text-secondary dark:hover:text-secondary/80"
-              href="https://www.smarttechtank.com"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Smart Tech Tank
-            </a>
-          </p>
-        </AccordionTrigger>
-        <AccordionContent>
           <h2 className="text-sm font-semibold text-primary dark:text-secondary">
             Check URL for free
           </h2>
+        </AccordionTrigger>
+        <AccordionContent>
           <p className="text-sm text-muted-foreground dark:text-muted/70">
             URL Checker is an extension that allows you to check URLs for
             malicious content. Use it for your own safety.
